defb328d0d20af748d554ff30f87f0b1874b4d39
Ian Carlos <carlos.ian007@gmail.com>
2019-03-25 18:26:51 -0300

    Correcting Bugs on Server

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Routerrq();
+const router = express.Router();
 const multer = require("multer");
 const multerConfig = require("../config/multer");
 
