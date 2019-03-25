ebb819750d084a5ceb582e410d2639bbfe38c1b4
Ian Carlos <carlos.ian007@gmail.com>
2019-03-25 14:55:32 -0300

    Adding styled-components

--- client/src/components/upload/styles.js
+++ client/src/components/upload/styles.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Upload extends Component {
+export default class Styles extends Component {
   render() {
     return <div />;
   }
--- server/src/index.js
+++ server/src/index.js
@@ -24,5 +24,5 @@ app.use(
 
 app.use(require("./routes/routes"));
 
-const port = 3000;
+const port = 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
