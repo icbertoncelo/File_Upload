cda8e1ec23f558ec6386aa4608abcb8596ee6adc
Ian Carlos <carlos.ian007@gmail.com>
2019-03-25 15:25:11 -0300

    Adding Upload Styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,15 @@ import React, { Component } from "react";
 import GlobalStyle from "./styles/global";
 import { Container, Content } from "./styles";
 
+import Upload from "./components/upload/Upload";
+
 class App extends Component {
   render() {
     return (
       <Container>
-        <Content>Teste</Content>
+        <Content>
+          <Upload />
+        </Content>
         <GlobalStyle />
       </Container>
     );
--- client/src/components/upload/Upload.js
+++ client/src/components/upload/Upload.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 
+// Upload Box
+import Dropzone from "react-dropzone";
+
+// Styles
+import { DropContainer, UploadMessage } from "./styles";
+
 export default class Upload extends Component {
   render() {
-    return <div />;
+    return (
+      <Dropzone accept="image/*" onDropAccepted={() => {}}>
+        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer
+            {...getRootProps()}
+            isDragActive={isDragActive}
+            isDragReject={isDragReject}
+          >
+            <input {...getInputProps()} />
+            Drop your files here
+          </DropContainer>
+        )}
+      </Dropzone>
+    );
   }
 }
