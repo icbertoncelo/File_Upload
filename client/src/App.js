191782a489dca324211f9ddb2726551cb229a5e0
Ian Carlos <carlos.ian007@gmail.com>
2019-03-25 18:36:14 -0300

    Adding FileList Component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import GlobalStyle from "./styles/global";
 import { Container, Content } from "./styles";
 
 import Upload from "./components/upload/Upload";
+import FileList from "./components/fileList/FileList";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <Container>
         <Content>
           <Upload />
+          <FileList />
         </Content>
         <GlobalStyle />
       </Container>
